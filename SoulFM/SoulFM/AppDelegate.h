3e2d6ffd4e249ab170c31232661cd26d649cee4a
ljh <lijh@test.com>
2015-12-09 11:10:21 +0800

    初始化工程

---
+++ SoulFM/SoulFM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SoulFM
+//
+//  Created by lijinghua on 15/12/9.
+//  Copyright © 2015年 lijinghua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SoulFM/SoulFM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SoulFM
+//
+//  Created by lijinghua on 15/12/9.
+//  Copyright © 2015年 lijinghua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
